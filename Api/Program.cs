268316b208ea9365d4d9d12ca42b130d462e6ce5
Danilo Florenzano <danilo.maia@aedb.br>
2023-08-17 23:35:30 -0300

    Containerize api and blazor app

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,9 @@ using Core;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}/");
+
 var movies = FakeData.Movies;
 
 app.MapGet("/movies", () => movies);
